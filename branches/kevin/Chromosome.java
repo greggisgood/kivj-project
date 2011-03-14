9cf6ac4d7db12cea8578aa395d9e0cf7c4990010
kevinlinjx@gmail.com <kevinlinjx@gmail.com@04f66712-d80a-34a1-8930-790ee3a6671e>
2011-03-14 16:35:39 +0000

    Genetic Algorithm basically set up but only performing at 5.3k rows so far. Need more heuristics :)

--- branches/kevin/Chromosome.java
+++ branches/kevin/Chromosome.java
@@ -4,11 +4,16 @@ import java.util.*;
 public class Chromosome 
 {
 	public static final int GENE_LENGTH = 8;
-	public static final int CHROMOSOME_LENGTH = 4;
+	public static final int CHROMOSOME_LENGTH = 5;
 	
 	public static final double MUTATION = 0.001; //Chance for a bit to mutate
 	
 	protected int [][] chromosome = new int[CHROMOSOME_LENGTH][GENE_LENGTH];
+	
+	private int actualWeight = 0;
+	private String completeChromosome = "";
+	
+	Random random = new Random();
 	
 	public Chromosome()
 	{	
@@ -17,13 +22,14 @@ public class Chromosome
 		{
 			seedGene(i);
 		}
-		System.out.println();
+		//System.out.println();
+		//System.out.println("Complete Chromosome: " + completeChromosome);
 	}
 	
 	public void seedGene(int geneNum)
 	{
 		String completeGene = "";
-		Random random = new Random();
+
 		//int gene = random.nextInt(101 - 1) + 1; //Upper limit is exclusive, lower limit is inclusive
 		
 		for (int j = 0; j < GENE_LENGTH; j++)
@@ -42,12 +48,49 @@ public class Chromosome
 			completeGene += chromosome[geneNum][j];
 		}
 		
+		completeChromosome += completeGene;
+		
 		//System.out.print(completeGene + " ");
 	}
 	
-	public void mutate()
+	public Chromosome mutate()
 	{
 		//Check if any bit mutates
+		for(int i = 0; i < 32; i++)
+		{
+			double check  = random.nextDouble();
+			
+			if (check < MUTATION)
+			{
+				System.out.println("Before: " + completeChromosome);
+				int position = chromosome[(i)/8][(i)%8];
+				if (position == 0)
+				{
+					chromosome[(i)/8][(i)%8] = 1;
+				}
+				else if (position == 1)
+				{
+					chromosome[(i)/8][(i)%8] = 0;
+				}
+				
+				String completeGene = "";
+				
+				for (int j = 0; j < CHROMOSOME_LENGTH; j++)
+				{
+					for (int k = 0; k < GENE_LENGTH; k++)
+					{
+						completeGene += chromosome[j][k];
+					}
+					//weights[i] = Integer.parseInt(completeGene, 2);
+					//System.out.println(weights[i]);
+					
+					completeChromosome += completeGene;
+					completeGene = "";
+				}
+			}
+		}
+		
+		return this;
 	}
 	
 	public int[] getWeights()
@@ -69,5 +112,23 @@ public class Chromosome
 		}
 		
 		return weights;
+	}
+	
+	public String getChromosome()
+	{
+		return completeChromosome;
+	}
+	
+	public void setChromosome(String newSequence)
+	{
+		completeChromosome = newSequence;
+	}
+	
+	public void setActualWeight(int newWeight)
+	{
+		System.out.println("newWeight: " + newWeight);
+		//actualWeight = newWeight;
+		completeChromosome = Integer.toString(newWeight);
+		System.out.println("completeChromosome: " + completeChromosome);
 	}
 }
