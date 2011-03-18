d54b9f17f2edfbdd8708ae3703a62dd1ff3cf7aa
urnujil@gmail.com <urnujil@gmail.com@04f66712-d80a-34a1-8930-790ee3a6671e>
2011-03-18 14:49:01 +0000

    Added more features, scoring about 5k to 300k

--- branches/junru/ParticleSwarm.java
+++ branches/junru/ParticleSwarm.java
@@ -1,36 +1,34 @@
+import java.io.*;
 import java.util.*;
+
 public class ParticleSwarm {
 	public static final int MAXBOUND = 100; // Max range of Weights to test
 	public static final int MINBOUND = 0;
 	public static final int RANGE = MAXBOUND - MINBOUND;
 	public static final int VELOCITYINIT = RANGE;
-	public static final int FEATURECOUNT = 9; // Number of features to assign weights to
-	public static final int SWARMSIZE = 53;//203; // Number of swarm particles
+	public static final int FEATURECOUNT = 13; // Number of features to assign weights to
+	public static final int SWARMSIZE = 63;//203; // Number of swarm particles
 	public static final int ITERATION = 50;
-	public static final int REPETITION = 3;
-	
+	public static final int REPETITION = 1;
+	public static final String FILENAME = "particle.txt";
+
 	private static Random rand = new Random(); // Random number generator
 	private static Particle[] particles;
 	private static int gBest= 0; // Best global score
 	private static double [] gBestWeight=new double[FEATURECOUNT]; // Weights to get gBest score
-	
+
 	public static void main(String[] args) {
 		initSwarm();
-		int score = 0;
 		for (int iter = 0; iter <ITERATION; iter++)
 		{
 			for (int i = 0; i < particles.length; i++)
 			{
+				int score = Integer.MAX_VALUE;
 				for (int j= 0; j < REPETITION ; j++)
 				{
-					score += evaluate(particles[i]);
+					// Find min in several reps for pessimistic evaluation
+					score = Math.min(evaluate(particles[i]), score);
 				}
-				score = score/REPETITION;
-			/*	for (int j = 0; j< particles[i].getWeights().length; j++)
-				{
-					System.out.print(particles[i].getWeights()[j] + " ");
-				}
-				System.out.println();*/
 				particles[i].updateScore(score);
 				if (score > gBest)
 				{
@@ -40,16 +38,16 @@ public class ParticleSwarm {
 				particles[i].updateVelocity(gBestWeight, rand, VELOCITYINIT);
 				particles[i].updatePosition(MAXBOUND, MINBOUND);
 			}
-			System.out.print("Best Score in iter" +iter + " " + gBest);
+			System.out.print("Best Score in iter" +iter + " " + gBest+ " ");
 			for (int k = 0; k< gBestWeight.length; k++)
 				System.out.print(gBestWeight[k] + " ");
 			System.out.println();
 		}
-		System.out.print("Best Score " + gBest);
+		System.out.print("Best Score " + gBest + " ");
 		for (int i = 0; i< gBestWeight.length; i++)
 			System.out.print(gBestWeight[i] + " ");
 		System.out.println();
-	/*	for (int i = 0; i < particles.length; i++)
+		/*	for (int i = 0; i < particles.length; i++)
 		{
 			System.out.print("Weights: ");
 			for (int j = 0; j< particles[i].getWeights().length; j++)
@@ -63,24 +61,50 @@ public class ParticleSwarm {
 			}
 			System.out.println();
 		}*/
-		
+
+		BufferedWriter bufferedWriter = null;
+
+		try {
+			//Construct the BufferedWriter object
+			bufferedWriter = new BufferedWriter(new FileWriter(FILENAME));
+
+			//Start writing to the output stream
+			bufferedWriter.append(gBest + " ");
+			for (int i = 0; i< gBestWeight.length; i++)
+				bufferedWriter.append(gBestWeight[i] + " ");
+			bufferedWriter.append("\n");
+		} catch (FileNotFoundException ex) {
+			ex.printStackTrace();
+		} catch (IOException ex) {
+			ex.printStackTrace();
+		} finally {
+			//Close the BufferedWriter
+			try {
+				if (bufferedWriter != null) {
+					bufferedWriter.flush();
+					bufferedWriter.close();
+				}
+			} catch (IOException ex) {
+				ex.printStackTrace();
+			}
+		}
 	}
 
 	// Plays a single game and using a weights of the particle and returns score
 	private static int evaluate(Particle particle) {
 		State s = new State();
-	//	new TFrame(s);
+		//	new TFrame(s);
 		PlayerSkeleton p = new PlayerSkeleton();	
 		p.setWeights(particle.getWeights());
 		while(!s.hasLost()) {
 			s.makeMove(p.pickMove(s,s.legalMoves()));
-		//	s.draw();
-		//	s.drawNext(0,0);
-	//		try {
+			//	s.draw();
+			//	s.drawNext(0,0);
+			//		try {
 			//	Thread.sleep(300);
-		//	} catch (InterruptedException e) {
-		//		e.printStackTrace();	
-		//	}
+			//	} catch (InterruptedException e) {
+			//		e.printStackTrace();	
+			//	}
 		}
 		//System.out.println("You have completed "+s.getRowsCleared()+" rows.");
 		return s.getRowsCleared();
