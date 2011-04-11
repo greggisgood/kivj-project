07badd84510fddd7b1ef49b04eb2818bf1d79e16
urnujil <urnujil@gmail.com>
2011-04-11 00:58:14 +0000

    Code cleanup

--- PlayerSkeleton.java
+++ PlayerSkeleton.java
@@ -1,33 +1,38 @@
 import java.io.*;
-import java.io.FileNotFoundException;
-import java.io.FileWriter;
-import java.io.IOException;
 
 public class PlayerSkeleton {
 	
-	
+	// Duplicate Fields from State, used in simulating of possible next move
 	protected int [][] field = new int[State.ROWS][State.COLS];
 	protected int [] top = new int[State.COLS];
 	protected double[] weights;
 	protected int nextPiece;
 	protected int turn;
 	// Following variables are features
-	protected int height; // Landing Height
-	protected int totalHeight;
-	protected int diffHeight;
-	protected int altitudeDiff;
-	protected int maxHeight;
-	protected int rowsCleared; // Rows cleared by a Single Move, not total rows cleared
-	protected int weightedSum;
-	protected int holeCount;
-	protected int connectedHole;
-	protected int maxWellDepth;
-	protected int sumWells;
-	protected int weightedWells;
-	protected int hTransition;
-	protected int vTransition;
+	protected int height; 		// Landing Height of piece
+	protected int totalHeight;	// Sum of Heights of all columns
+	protected int diffHeight;	// Sum of difference of adjacent columns
+	protected int altitudeDiff;	// Difference between max and min column
+	protected int maxHeight;	// Height of highest column
+	protected int rowsCleared; 	// Rows cleared by a Single Move, not total rows cleared
+	protected int weightedSum;	// Sum of number of filled cells, weighted by the height of the cell 
+	protected int holeCount;	// Number of empty cells with at least 1 filled cell above in same column
+	protected int connectedHole;// Number of holes, vertically connected holes are considered as 1
+	protected int maxWellDepth; // Max depth of well (depression on the top surface of the blocks)
+	protected int sumWells;		// Sum of depths of all wells
+	protected int weightedWells; // Sum of depths of wells,weighted(depth 4 well has weight 1+2+3+4)
+	protected int hTransition;   // Number of transitions(filled -> empty cell and vice versa) horizontally
+	protected int vTransition;  // Number of transitions vertically
 	protected int erodedCells; // rowsCleared * number of cells cleared belonging to this turn's piece
-
+	
+	// Best set of weights obtained from experiments
+	protected static double[] getBestWeights()
+	{
+		double [] w = {11.708585390569464, 0.0, 1.8974325060818578, 0.0, 0.0, 100.0, 0.0, 83.40570775458364, 19.74020548619076, 49.12019352048655, 6.780747031944694, 35.93440126423564, 95.00893395888016, 6.4556750383596775, 19.070132593850886};
+		return w;
+	}
+	
+// Following code are taken from State.java, used in simulation possible next moves
 	//possible orientations for a given piece type
 	protected static int[] pOrients = {1,2,4,4,4,2,2};
 	
@@ -71,199 +76,9 @@ public class PlayerSkeleton {
 		{{2,2,1},{2,3}}
 	};
 	
-	//implement this function to have a working system
-	public int pickMove(State s, int[][] legalMoves) {
-		nextPiece = s.getNextPiece();
-		turn = s.getTurnNumber();
-		int bestMove = 0; // stores best move so far
-		int bestHeuristics = Integer.MAX_VALUE;
-		int currentHeuristics;
-		for (int i = 0; i < legalMoves.length; i++)
-		{
-			for (int j=0; j <State.ROWS ;j++) {
-				System.arraycopy(s.getField()[j],0, field[j],0, State.COLS);
-			}
-			System.arraycopy(s.getTop(), 0, top, 0, s.getTop().length);
-			if (makeMove(legalMoves[i][0], legalMoves[i][1]))
-			{
-				currentHeuristics = getHeuristicsVal();
-				if (currentHeuristics < bestHeuristics) // Smaller heuristics = better
-				{
-					bestMove = i;
-					bestHeuristics = currentHeuristics;
-				}
-			}
-		}
-		return bestMove;
-	}
-	
-
-	public static void main2(String[] args) {
-
-	BufferedWriter bufferedWriter = null;
-	BufferedReader bufferedReader = null;
-	
-//	double [] w1 = { 39.87226246992793, 0.2822313301577022, 0.0, 0.0, 0.0, 90.41644192059289, 0.0, 100.0, 29.025892632632264, 57.261460721109174, 0.0, 26.175997125047157, 100.0, 4.939800183261108, 0};
-
-	double [] w2 = { 4.611666397824846, 0.0, 6.687078463473015, 0.0, 17.65034849478665, 100.0, 0.0, 99.63155660402734, 26.45383860283068, 1.7200906369830626, 22.454776188856126, 17.11024244496571, 44.18592529604314, 4.22881999848464, 5.29200858840683};
-
-//	double [] w3 = { 21.68486505002145, 0.0, 9.023652352698946, 0.0, 0.0, 95.86317187656553, 0.0, 93.16080358412056, 5.803678433049965, 50.91238805305449, 9.799377808831037, 29.916726437473535, 100.0, 22.881255312018595, 48.07887629107038}; 
-
-//	double [] w4 = { 0.0, 0.0, 0.0, 0.0, 1.5582433749262439, 91.8331292850514, 0.0, 98.1326665734521, 0.0, 61.78943974409394, 1.5444211711309312, 36.82438901150083, 100.0, 67.21811420862298, 0.11983310366898747 };
-	double [] w5 = { 22.71084633664169, 0.0, 0.0, 0.0, 0.0, 100.0, 0.0, 0.0, 0.0, 24.379420943978545, 17.406699232557568, 29.124462600339385, 100.0, 63.5542726066547, 0.0 };
-
-//	double [] w6 = {48.8552474545055, 0.0, 0.0, 0.0, 0.0, 86.82998862893662, 0.0, 81.59178548320465, 7.118188015109838, 72.82872916452304, 0.0, 16.887314224810783, 100.0, 91.71182159781006, 0.0 };
-	double [] w7 = {11.708585390569464, 0.0, 1.8974325060818578, 0.0, 0.0, 100.0, 0.0, 83.40570775458364, 19.74020548619076, 49.12019352048655, 6.780747031944694, 35.93440126423564, 95.00893395888016, 6.4556750383596775, 19.070132593850886};
-
-//	double [] w8 = {11.708585390569464, 0.0, 1.8974325060818578, 0.0, 0.0, 100.0, 0.0, 83.40570775458364, 19.74020548619076, 49.12019352048655, 6.780747031944694, 35.93440126423564, 95.00893395888016, 6.4556750383596775, 19.070132593850886};
-
-//	double [] w9 = {0.0, 0.6619439531101865, 4.793286917178153, 0.0, 0.0, 100.0, 0.0, 100.0, 24.342880914974636, 21.06145429004378, 20.750565667664198, 33.55267648693068, 63.19066654766899, 56.73427792398737, 0.0};
-
-//	double [] w10 = {61.88706839048098, 0.0, 0.0, 0.0, 2.830323893727921, 100.0, 0.0, 100.0, 0.0, 9.394831701193526, 32.371113940085, 17.889372940018198, 80.93552071247848, 8.288340692659537, 0.0};
-	
-	double [][] w = {w2,w5,w7};
-	long seed = 0;
-		for (int i = 0; i < w.length ; i++)			
-		{
-			int minscore =Integer.MAX_VALUE;
-			int totalscore = 0;
-
-			try {
-				bufferedReader = new BufferedReader(new FileReader("seeds.txt"));
-				String line;
-				while ((line = (bufferedReader.readLine())) != null) 
-				{
-					seed = Long.parseLong(line);
-					State s = new State();
-					s.setSeed(seed);
-					PlayerSkeleton p = new PlayerSkeleton(); 
-					p.setWeights(w[i]);
-					while(!s.hasLost()) {
-					s.makeMove(p.pickMove(s,s.legalMoves()));
-					}
-					System.out.println("Weights " + i + " Rows Cleared: " + s.getRowsCleared());
-					if (s.getRowsCleared() < minscore)
-					{
-						minscore = s.getRowsCleared();
-					}	
-					totalscore += s.getRowsCleared();
-					try {
-						//Construct the BufferedWriter object
-						bufferedWriter = new BufferedWriter(new FileWriter("results.txt", true));
-
-						//Start writing to the output stream
-						bufferedWriter.append("Weights" + i +": min score :" + minscore + " total score :" + totalscore + '\n');
-					} catch (FileNotFoundException ex) {
-						ex.printStackTrace();
-					} catch (IOException ex) {
-						ex.printStackTrace();
-					} finally {
-						//Close the BufferedWriter
-						try {
-							if (bufferedWriter != null) {
-								bufferedWriter.flush();
-								bufferedWriter.close();
-							}		
-						} catch (IOException ex) {
-							ex.printStackTrace();
-						}	
-					} // End of bw try
-				} // End of while
-			} // End of br try
-			catch (Exception e) {
-			       System.err.println("Error: " + e);
-		     	}
-			finally {
-				//Close the BufferedReader
-				try {
-				if (bufferedReader != null) {
-					bufferedReader.close();
-					}		
-				} catch (IOException ex) {
-					ex.printStackTrace();
-				}	
-			} // End Finally
-			
-		}
-	}
-	// Generating TestSeeds list, runs 20 iterations using a given weight 
-	// And writes hardest seed sequence (lowest score) to textfile
-	// The seed is then used in next run of particle swarm
-
-	public static void main3(String[] args) {
-		int min = Integer.MAX_VALUE;
-
-	double [] w = {0.0, 0.6619439531101865, 4.793286917178153, 0.0, 0.0, 100.0, 0.0, 100.0, 24.342880914974636, 21.06145429004378, 20.750565667664198, 33.55267648693068, 63.19066654766899, 56.73427792398737, 0.0};
-		long minSeed = 0;
-		for (int i = 0; i < 20 ; i++)			
-		{
-			State s = new State();
-			long seed = s.setRandomSeed();
-		//	long seed = 1300715528447L;
-		//	s.setSeed(seed);
-			System.out.println("Seed " + i + ": "  + seed);
-		//	new TFrame(s);
-			PlayerSkeleton p = new PlayerSkeleton(); 
-			p.setWeights(w);
-			while(!s.hasLost()) {
-				s.makeMove(p.pickMove(s,s.legalMoves()));
-			//	s.draw();
-			//	s.drawNext(0,0);
-			}
-			System.out.println("Rows Cleared: " + s.getRowsCleared());
-			if (s.getRowsCleared() < min)
-			{
-				min = s.getRowsCleared();
-				minSeed = seed;
-			}
-		//	System.out.println("You have completed "+s.getRowsCleared()+" rows.");
-		}
-		BufferedWriter bufferedWriter = null;
-
-		try {
-			//Construct the BufferedWriter object
-			bufferedWriter = new BufferedWriter(new FileWriter("seeds.txt", true));
-
-			//Start writing to the output stream
-			bufferedWriter.append(minSeed + "\n");
-		} catch (FileNotFoundException ex) {
-			ex.printStackTrace();
-		} catch (IOException ex) {
-			ex.printStackTrace();
-		} finally {
-			//Close the BufferedWriter
-			try {
-				if (bufferedWriter != null) {
-					bufferedWriter.flush();
-					bufferedWriter.close();
-				}
-			} catch (IOException ex) {
-				ex.printStackTrace();
-			}
-		}
-	}
-	
-	// Default playing mode, plays one random game with modified state.java
-	public static void main(String[] args) {
-		double [] w = {  22.71084633664169, 0.0, 0.0, 0.0, 0.0, 100.0, 0.0, 0.0, 0.0, 24.379420943978545, 17.406699232557568, 29.124462600339385, 100.0, 63.5542726066547, 0.0 };
-		State s = new State();
-		s.setRandomSeed();
-		//	long seed = 1300715528447L;
-		//	s.setSeed(seed);
-		//	new TFrame(s);
-		PlayerSkeleton p = new PlayerSkeleton(); 
-		p.setWeights(w);
-		while(!s.hasLost()) {
-			s.makeMove(p.pickMove(s,s.legalMoves()));
-			//	s.draw();
-			//	s.drawNext(0,0);
-		}
-		System.out.println("You have completed "+s.getRowsCleared()+" rows.");
-	}
-		
-	// Copy of makeMove in State
-	// Feature 13 - rows cleared in this move
-	// Feature 14 - Eroded Cells in this move
+	// Copy of makeMove
+	// Evaluates Feature 13 - rows cleared in this move
+	// Evaluates Feature 14 - Eroded Cells in this move
 	public boolean makeMove(int orient, int slot) {
 		erodedCells = 0;
 		rowsCleared = 0;
@@ -328,7 +143,173 @@ public class PlayerSkeleton {
 		erodedCells *= rowsCleared;
 		return true;
 	}
+// End of code duplicate from State.java
+
+	//implement this function to have a working system
+	public int pickMove(State s, int[][] legalMoves) { 
+		nextPiece = s.getNextPiece();
+		turn = s.getTurnNumber();
+		int bestMove = 0; 	// stores best move so far
+		int bestHeuristics = Integer.MAX_VALUE;
+		int currentHeuristics;
+		for (int i = 0; i < legalMoves.length; i++)
+		{
+			// Duplicate the current field information
+			for (int j=0; j <State.ROWS ;j++) {
+				System.arraycopy(s.getField()[j],0, field[j],0, State.COLS);
+			}
+			System.arraycopy(s.getTop(), 0, top, 0, s.getTop().length);
+			// Test out all possible moves
+			if (makeMove(legalMoves[i][0], legalMoves[i][1]))
+			{
+				currentHeuristics = getHeuristicsVal();
+				// Smaller heuristics = better
+				if (currentHeuristics < bestHeuristics)
+				{
+					bestMove = i;
+					bestHeuristics = currentHeuristics;
+				}
+			}
+		}
+		return bestMove;
+	}
 	
+
+// Plays using the set of seeds in seeds.txt and writes score into results.txt
+	public static void main(String[] args) {
+
+	BufferedWriter bufferedWriter = null;
+	BufferedReader bufferedReader = null;
+	
+	double [] w1 = { 39.87226246992793, 0.2822313301577022, 0.0, 0.0, 0.0, 90.41644192059289, 0.0, 100.0, 29.025892632632264, 57.261460721109174, 0.0, 26.175997125047157, 100.0, 4.939800183261108, 0};
+	double [] w2 = { 4.611666397824846, 0.0, 6.687078463473015, 0.0, 17.65034849478665, 100.0, 0.0, 99.63155660402734, 26.45383860283068, 1.7200906369830626, 22.454776188856126, 17.11024244496571, 44.18592529604314, 4.22881999848464, 5.29200858840683};
+//	double [] w3 = { 21.68486505002145, 0.0, 9.023652352698946, 0.0, 0.0, 95.86317187656553, 0.0, 93.16080358412056, 5.803678433049965, 50.91238805305449, 9.799377808831037, 29.916726437473535, 100.0, 22.881255312018595, 48.07887629107038}; 
+//	double [] w4 = { 0.0, 0.0, 0.0, 0.0, 1.5582433749262439, 91.8331292850514, 0.0, 98.1326665734521, 0.0, 61.78943974409394, 1.5444211711309312, 36.82438901150083, 100.0, 67.21811420862298, 0.11983310366898747 };
+//	double [] w5 = { 22.71084633664169, 0.0, 0.0, 0.0, 0.0, 100.0, 0.0, 0.0, 0.0, 24.379420943978545, 17.406699232557568, 29.124462600339385, 100.0, 63.5542726066547, 0.0 };
+//	double [] w6 = {48.8552474545055, 0.0, 0.0, 0.0, 0.0, 86.82998862893662, 0.0, 81.59178548320465, 7.118188015109838, 72.82872916452304, 0.0, 16.887314224810783, 100.0, 91.71182159781006, 0.0 };
+//	double [] w8 = {11.708585390569464, 0.0, 1.8974325060818578, 0.0, 0.0, 100.0, 0.0, 83.40570775458364, 19.74020548619076, 49.12019352048655, 6.780747031944694, 35.93440126423564, 95.00893395888016, 6.4556750383596775, 19.070132593850886};
+//	double [] w9 = {0.0, 0.6619439531101865, 4.793286917178153, 0.0, 0.0, 100.0, 0.0, 100.0, 24.342880914974636, 21.06145429004378, 20.750565667664198, 33.55267648693068, 63.19066654766899, 56.73427792398737, 0.0};
+//	double [] w10 = {61.88706839048098, 0.0, 0.0, 0.0, 2.830323893727921, 100.0, 0.0, 100.0, 0.0, 9.394831701193526, 32.371113940085, 17.889372940018198, 80.93552071247848, 8.288340692659537, 0.0};
+	double [] w7 = {11.708585390569464, 0.0, 1.8974325060818578, 0.0, 0.0, 100.0, 0.0, 83.40570775458364, 19.74020548619076, 49.12019352048655, 6.780747031944694, 35.93440126423564, 95.00893395888016, 6.4556750383596775, 19.070132593850886};
+ 	double [][] w = {w7};
+	
+	long seed = 0;
+		for (int i = 0; i < w.length ; i++)			
+		{
+			try {
+				bufferedReader = new BufferedReader(new FileReader("seeds.txt"));
+				String line;
+				while ((line = (bufferedReader.readLine())) != null) 
+				{
+					seed = Long.parseLong(line);
+					State s = new State();
+					s.setSeed(seed);
+					PlayerSkeleton p = new PlayerSkeleton(); 
+					p.setWeights(w[i]);
+					while(!s.hasLost()) {
+						s.makeMove(p.pickMove(s,s.legalMoves()));
+					}			
+					System.out.println("Rows Cleared: " + s.getRowsCleared());
+					try {
+						//Construct the BufferedWriter object
+						bufferedWriter = new BufferedWriter(new FileWriter("results.txt", true));
+
+						//Start writing to the output stream
+						bufferedWriter.append("" + s.getRowsCleared() + '\n');
+					} catch (Exception ex) {
+						ex.printStackTrace();
+					} finally {
+						//Close the BufferedWriter
+						try {
+							if (bufferedWriter != null) {
+								bufferedWriter.flush();
+								bufferedWriter.close();
+							}		
+						} catch (IOException ex) {
+							ex.printStackTrace();
+						}	
+					} // End of bw try
+				} // End of while
+			} // End of br try
+			catch (Exception e) {
+			       System.err.println("Error: " + e);
+		     	}
+			finally {
+				//Close the BufferedReader
+				try {
+				if (bufferedReader != null) {
+					bufferedReader.close();
+					}		
+				} catch (IOException ex) {
+					ex.printStackTrace();
+				}	
+			} // End Finally
+		}
+	}
+	
+	// Generating TestSeeds list, runs 20 iterations using a given weight 
+	// And writes hardest seed sequence (lowest score) to textfile
+	// The seed is then used in next run of particle swarm
+	public static void getRandomSeed(String[] args) {
+		int min = Integer.MAX_VALUE;
+		long minSeed = 0;
+		for (int i = 0; i < 20 ; i++)			
+		{
+			State s = new State();
+			long seed = s.setRandomSeed();
+		//	long seed = 1300715528447L;
+		//	s.setSeed(seed);
+			System.out.println("Seed " + i + ": "  + seed);
+			PlayerSkeleton p = new PlayerSkeleton(); 
+			p.setWeights(PlayerSkeleton.getBestWeights());
+			while(!s.hasLost()) {
+				s.makeMove(p.pickMove(s,s.legalMoves()));
+			}
+			System.out.println("Rows Cleared: " + s.getRowsCleared());
+			if (s.getRowsCleared() < min) // Update new minimum seed
+			{
+				min = s.getRowsCleared();
+				minSeed = seed;
+			}
+		}
+		BufferedWriter bufferedWriter = null;
+		try {
+			//Construct the BufferedWriter object
+			bufferedWriter = new BufferedWriter(new FileWriter("seeds.txt", true));
+			//Start writing to the output stream
+			bufferedWriter.append(minSeed + "\n");
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}  finally {
+			//Close the BufferedWriter
+			try {
+				if (bufferedWriter != null) {
+					bufferedWriter.flush();
+					bufferedWriter.close();
+				}
+			} catch (IOException ex) {
+				ex.printStackTrace();
+			}
+		}
+	}
+	
+	// Default playing mode, plays one random game with modified state.java
+	public static void main2(String[] args) {
+		State s = new State();
+		s.setRandomSeed();
+		//	long seed = 1300715528447L;
+		//	s.setSeed(seed);
+		new TFrame(s);
+		PlayerSkeleton p = new PlayerSkeleton(); 
+		p.setWeights(PlayerSkeleton.getBestWeights());
+		while(!s.hasLost()) {
+			s.makeMove(p.pickMove(s,s.legalMoves()));
+			s.draw();
+			s.drawNext(0,0);
+		}
+		System.out.println("You have completed "+s.getRowsCleared()+" rows.");
+	}
+		
 	// Sets weights for the heuristics
 	public void setWeights(double[] newWeights)
 	{
@@ -338,6 +319,7 @@ public class PlayerSkeleton {
 	// Calculates and returns heuristics value
 	public int getHeuristicsVal()
 	{
+		// Call functions to evaluate the features
 		checkHeights();
 		checkHoles();
 		checkWells();
@@ -487,6 +469,5 @@ public class PlayerSkeleton {
 			}
 		}
 	}
-	
-	
+		
 }
